09d1cc77035c282e61fb583b44d25efa7a3f37f9
filipkgsson <filip.kgsson@gmail.com>
2018-04-28 18:48:07 +0200

    Förbättrad kod

--- Saver.java
+++ Saver.java
@@ -8,55 +8,42 @@ public class Saver {
 		count = 0;
 	}
 
-	public String save(Object o) throws NoSuchFieldException, SecurityException, IllegalArgumentException,
-			IllegalAccessException, InvocationTargetException {
+	public String save(Object o) throws IllegalArgumentException, InvocationTargetException, IllegalAccessException {
 		String xml = "";
 		Class<?> clazz = o.getClass();
 		Element e = clazz.getDeclaredAnnotation(Element.class);
-		if (e != null) {
-			for(int i = 0; i < count; i++)
-				xml += " ";
-			xml += "<" + e.name() + " ";
-			String s = "";
-			String end = "";
-			for (Method m : clazz.getDeclaredMethods()) {
-				SubElements sub = m.getDeclaredAnnotation(SubElements.class);
-				if (sub != null) {
-					Field f = clazz.getDeclaredField("children");
-					f.setAccessible(true);
-					if (f.getType().isArray()) {
-						Object[] children = (Object[]) f.get(o);
-						if (children != null) {
-							count ++;
-							s += "\n";
-							for(int i = 0; i < count; i++)
-								s += " ";
-							s += "<" + sub.name() + ">";
-							for (Object child : children) {
-								//Saver temp = new Saver();
-								s += "\n " + this.save(child);
-							}
-							s += "\n";
-							count --;
-							String space = "";
-							for(int i = 0; i < count; i++)
-								s += " ";
-							s += " ";
-							s += "</" + sub.name() + ">\n" + space + "</" + e.name() + ">";
-						}
-						else
-							end = "/";
-					}
-				}
-				ElementField ef = m.getDeclaredAnnotation(ElementField.class);
-				if (ef != null) {
-					Object value = m.invoke(o);
-					xml += ef.name() + "=" + "\"" + value + "\"";
+		if (e == null)
+			return "";
+		xml += createSpace(count) + "<" + e.name() + " ";
+		String s = "";
+		for (Method m : clazz.getDeclaredMethods()) {
+			SubElements sub = m.getDeclaredAnnotation(SubElements.class);
+			if (sub != null) {
+				Object[] children = (Object[]) m.invoke(o);
+				if (children != null) {
+					count++;
+					s += "\n" + createSpace(count) + "<" + sub.name() + ">";
+					for (Object child : children)
+						s += "\n " + this.save(child);
+					s += "\n" + createSpace(count) + "</" + sub.name() + ">\n" + "</" + e.name() + ">";
+					count--;
 				}
 			}
-			xml += end + ">";
-			xml += s;
+			ElementField ef = m.getDeclaredAnnotation(ElementField.class);
+			if (ef != null)
+				xml += ef.name() + "=" + "\"" + m.invoke(o) + "\"";
 		}
+		if(s.equals(""))
+			xml += "/";
+		xml += ">" + s;
 		return xml;
 	}
+	
+	private static String createSpace(int n) {
+		String space = "";
+		for (int i = 0; i < n; i++)
+			space += " ";
+		return space;
+	}
+	
 }
